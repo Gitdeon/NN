f599afeda72b7550e5fd7cc57df0c39e809c9307
Gitdeon <Gitdeon@github.com>
2019-03-04 15:18:19 +0100

    Combined all tasks so far in Assignment1.py

--- Assignment 1/Assignment1.py
+++ Assignment 1/Assignment1.py
@@ -183,7 +183,7 @@ for met in mets:
 
 # In[ ]:
 
-# <b> Task 3, Bayes classifier: Asymmetry idea </b>
+# <b> Task 3, Bayes classifier: Asymmetry feature </b>
 
 rows, columns = train_in.shape
 
@@ -239,6 +239,7 @@ for i in range(len(test_in)):
       X = X + sum(abs(test_in[i][range(x-8,x)] - list(reversed(test_in[i][range(x, x+8)]))))
       
    X = np.digitize(X, bins)   
+   
    #Calculate P(X=x|one)
    one_count = np.count_nonzero(one_bins == X)
    P_X_given_one = one_count / amount_ones
@@ -256,7 +257,7 @@ for i in range(len(test_in)):
       bayesian_out = np.append(bayesian_out, 1)
    else: bayesian_out = np.append(bayesian_out, 5)
 
-ones_array = bayesian_out[np.where(test_out == 1)] 
+ones_array = bayesian_out[np.where(test_out == 1)] #obtain indices
 fives_array = bayesian_out[np.where(test_out == 5)] 
 
 print("Correctly classified ones:", np.count_nonzero(ones_array == 1), "/", len(ones_array))
@@ -274,6 +275,101 @@ for i in range(10):
    print('Average asymmetry value for', i, ': ', Asymmetry_value/amount)
    
 '''
+
+# In[ ]:
+
+# <b> Task 4: Implement a multi-class perceptron algorithm </b>
+
+def activation(input_vec, weight_vec): #uses 
+    summation = np.dot(input_vec, weight_vec[1:]) + weight_vec[0]
+    #step function
+    if summation > 0:
+        return 0 #correctly classified
+    else:
+        return 1 #incorrectly classified
+
+class perceptron(object):
+    
+    def __init__(self, len_input, threshold, learning_rate):
+        self.len_input = len_input
+        self.threshold = threshold
+        self.learning_rate = learning_rate
+        #need a vector of weights for each digit 0-9, initialize randomly
+        self.weights = np.random.rand(len_input+1, 10) 
+        
+    def train(self, inputs, outputs):
+        count = 0
+        while count < self.threshold:
+            for input_vec, output in zip(inputs, outputs):
+                weight_vec = self.weights[:,output]
+                #determine if weights need to be adjusted
+                activation_w_x = activation(input_vec, weight_vec) 
+                self.weights[1:, output] += learning_rate * activation_w_x * input_vec
+                self.weights[0, output] += learning_rate * activation_w_x
+            count += 1
+        
+        return self.weights
+
+len_input, threshold, learning_rate = len(train_in[0]), 200, 0.01
+
+#sets up weights matrix on which the MNIST data can be applied
+W = perceptron(len_input, threshold, learning_rate)
+
+#applies the MNIST training data to find the weights
+W_trained = W.train(train_in, train_out)
+
+def get_output(inputs):
+    outputs_from_W = []
+    for x in inputs:
+        w_dot_x = np.zeros(10) #finding the maximum value of \vec{w}\cdot\vec{x}
+        for i in range(10): #iterating through the digits
+            weight_vec = W_trained[:,i]
+            w_dot_x[i] = (np.dot(x, weight_vec[1:]) + weight_vec[0])
+        classification = np.argmax(w_dot_x)
+        outputs_from_W.append(classification)
+    return outputs_from_W
+
+def confusion(actual, predicted, title):
+    
+    c = np.zeros([10,10])
+
+    rows = len(actual)
+
+    for k in range(rows):
+        i, j = actual[k], predicted[k]
+        c[i,j] += 1
+
+    got_it_right = 0
+    for j in range(10):
+        got_it_right += c[j,j] 
+
+    perc_error = str(round(int(rows - got_it_right)/float(rows)*100, 2))
+
+    ticks = [0,1,2,3,4,5,6,7,8,9]
+
+    plt.figure()
+    plt.title('%s Data, Error = %s percent'%(title, perc_error))
+    plt.xlabel('%s Output'%title)
+    plt.ylabel('Classification')
+    plt.xticks(ticks)
+    plt.yticks(ticks)
+    plt.imshow(c, origin = 'lower', interpolation='nearest', aspect = 'equal', cmap='binary')
+    plt.colorbar()
+
+    plt.savefig('Task4_%s.pdf'%title)
+
+train_outputs_from_W = get_output(train_in)
+test_outputs_from_W = get_output(test_in)
+
+confusion(train_out, train_outputs_from_W, 'Training')
+confusion(test_out, test_outputs_from_W, 'Testing')
+
+# In[ ]:
+
+# <b> Task 5: Implement the gradient descent algorithm </b>
+
+
+
 
 
 
